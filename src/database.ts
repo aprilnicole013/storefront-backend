9d4e4375629833397770fd77b55e53e18861b8d5
April <aprilnicole013@gmail.com>
2021-11-16 22:24:16 -0500

    Refactor environment variables

--- src/database.ts
+++ src/database.ts
@@ -4,18 +4,18 @@ import { Pool } from 'pg'
 dotenv.config()
 
 const {
-    POSTGRES_HOST,
-    POSTGRES_DB,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD
+    DEV_POSTGRES_HOST,
+    DEV_POSTGRES_DB,
+    DEV_POSTGRES_USER,
+    DEV_POSTGRES_PASSWORD
 } = process.env
 
 //connection to the db
 const database = new Pool ({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD
+    host: DEV_POSTGRES_HOST,
+    database: DEV_POSTGRES_DB,
+    user: DEV_POSTGRES_USER,
+    password: DEV_POSTGRES_PASSWORD
 })
 
 export default database
